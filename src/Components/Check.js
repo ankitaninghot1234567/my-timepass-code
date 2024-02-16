66f9647af9e497a621708af2b6e19be32ba8471f
ankitaninghot1234567 <ankita.awscloud@gmail.com>
2024-02-16 13:11:38 +0530

    react

---
+++ src/Components/Check.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Check(){
+    return(
+        <div>
+            <h1>
+            hi this for testing 2
+            </h1>
+        </div>
+    )
+}
---
+++ src/Components/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Heading(){
+    return(
+        <div>
+<h1>
+            hi this for testing 
+            </h1>
+        </div>
+    )
+}
